33c60b79fae90e971bc8addfcc32349b7742b368
Izzat <studiomegaapp@gmail.com>
2020-02-17 04:20:47 +0600

    initial commit

---
+++ kenguroo_partner/android/app/src/main/java/kg/kenguroo/kenguroo_partner/MainActivity.java
@@ -0,0 +1,13 @@
+package kg.kenguroo.kenguroo_partner;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
